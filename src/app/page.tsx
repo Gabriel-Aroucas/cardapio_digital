a11aa1545acf91313f28ad0f92c74548632de2dc
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-12-10 21:28:54 -0300

    feat: loader component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import './Page/page.css'
 import Header from '@/components/header/Header'
 import './Global _style/globals.css'
+import Loader from '@/components/Loader/Loader'
 export default function Home() {
   return(
     <section>
+      <Loader/>
       <Header pagina="Início" />
     </section>
   )
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loader.css"
+const Loader = () => {
+  return (
+    <section className="loader">
+      <div>
+      </div>
+      <h1>Carregando...</h1>
+    </section>
+  );
+};
+
+export default Loader;
