e1b338dff068705ad5c421afee5ba224ba94f044
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-12-12 22:11:07 -0300

    feat: styles for pricipal page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import "./Global _style/globals.css";
 import Loader from "@/components/Loader/Loader";
 import { useEffect, useRef, useState } from "react";
 export default function Home() {
- const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   const full_load_page_check = async () => {
     await fetch("/").then(() => {
@@ -20,6 +20,23 @@ export default function Home() {
     <section>
       {loading == true && <Loader />}
       <Header pagina="Início" />
+      <div className="container">
+        <img src="https://placehold.co/150x150" alt="business logo" />
+        <div className="info">
+          <h1>Lanchocho</h1>
+          <i>prazer em te servir</i>
+          <p>Endereço: rua f ficticio Nº 502</p>
+          <p>Tel: 9999999999</p>
+          <p>
+            Status de funcionamento: <span>x</span>
+          </p>
+        </div>
+        <div className="buttons">
+          <button type="button"> Cardápio</button>
+          <button type="button" disabled> Reservar uma mesa</button>
+          <button type="button" disabled>Reservar o espaço</button>
+        </div>
+      </div>
     </section>
   );
 }
