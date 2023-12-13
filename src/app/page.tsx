ef38e4132ba38c25d5504a50617d6af97c155b7e
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-12-12 21:42:24 -0300

    feat: added a loading page while principal page complete the load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
-import './Page/page.css'
-import Header from '@/components/header/Header'
-import './Global _style/globals.css'
-import Loader from '@/components/Loader/Loader'
+"use client";
+import "./Page/page.css";
+import Header from "@/components/header/Header";
+import "./Global _style/globals.css";
+import Loader from "@/components/Loader/Loader";
+import { useEffect, useRef, useState } from "react";
 export default function Home() {
-  return(
+ const [loading, setLoading] = useState(true)
+
+  const full_load_page_check = async () => {
+    await fetch("/").then(() => {
+      setLoading(false);
+    });
+  };
+  useEffect(() => {
+    full_load_page_check();
+  }, []);
+
+  return (
     <section>
-      <Loader/>
+      {loading == true && <Loader />}
       <Header pagina="Início" />
     </section>
-  )
+  );
 }
